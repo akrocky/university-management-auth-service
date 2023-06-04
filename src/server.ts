cc21e9564c3d8e69a62aad7dc047619ef8370640
akrocky <abulkashemrocky@gmail.com>
2023-06-04 23:23:55 +0200

    [Done] setup express, mongoose,corse

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import config from "./config"
+
+import app from "./app";
+
+
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database is connected successfully`);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+          })
+    } catch (err) {
+        console.log('Failed to connect database',err);
+    }
+    
+  
+   
+  }
+
+  bootstrap();
